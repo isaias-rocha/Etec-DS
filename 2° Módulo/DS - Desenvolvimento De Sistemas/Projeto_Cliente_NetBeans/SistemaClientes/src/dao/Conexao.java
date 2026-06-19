295e810a235b9106c7d7dd10c6c5b943cb12d81f
unknown <isaias_3642@UIRAPURU.BR>
2026-06-18 21:51:42 -0300

    feat: criando um novo projeto na aula do rubens chamado Sistema Clientes

---
+++ 2° Módulo/DS - Desenvolvimento De Sistemas/Projeto_Cliente_NetBeans/SistemaClientes/src/dao/Conexao.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author isaias_3642
+ */
+
+public class Conexao {
+
+    private static final String URL     = "jdbc:mysql://localhost:3306/curso_java";
+    private static final String USUARIO = "root";
+    private static final String SENHA   = "";
+
+    public static Connection conectar() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USUARIO, SENHA);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver não encontrado: " + e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("Erro ao conectar: " + e.getMessage());
+        }
+        return null;
+    }
+}
+    
