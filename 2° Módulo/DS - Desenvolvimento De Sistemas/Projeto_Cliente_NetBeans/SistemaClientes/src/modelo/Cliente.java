351654cc1a20b90172a07ecad44aec98ab085750
isaias-rocha <isaiasrocha.dev@gmail.com>
2026-06-25 18:48:59 -0300

    atualizando o rep

--- 2° Módulo/DS - Desenvolvimento De Sistemas/Projeto_Cliente_NetBeans/SistemaClientes/src/modelo/Cliente.java
+++ 2° Módulo/DS - Desenvolvimento De Sistemas/Projeto_Cliente_NetBeans/SistemaClientes/src/modelo/Cliente.java
@@ -10,4 +10,109 @@ package modelo;
  */
 public class Cliente {
     
+  private Integer id;
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String email;
+    private String cidade;
+    
+    // Construtor vszio
+    public Cliente () {}
+    
+    // Construtor completo
+    public Cliente (Integer id, String nome, String cpf, String telefone, String email, String cidade) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.email = email;
+        this.cidade = cidade;
+    }
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return the telefone
+     */
+    public String getTelefone() {
+        return telefone;
+    }
+
+    /**
+     * @param telefone the telefone to set
+     */
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the cidade
+     */
+    public String getCidade() {
+        return cidade;
+    }
+
+    /**
+     * @param cidade the cidade to set
+     */
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+    
+  
+    
 }
