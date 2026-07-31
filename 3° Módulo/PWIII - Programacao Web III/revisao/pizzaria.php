3e2d6b2b0135ea980e7215a0574eafcbb76958bd
unknown <ISAIAS_3642@UIRAPURU.BR>
2026-07-30 22:04:50 -0300

    aula de pwIII - projeto calculadora e Pizzaria

---
+++ 3° Módulo/PWIII - Programacao Web III/revisao/pizzaria.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="css/pizzaria.css">
+</head>
+<body>
+    
+<ul>
+    <li><a href="#"></a>Home</li>
+    <li><a href="#"></a>calculadora</li>
+    <li><a href="#"></a>estoque</li>
+    <li><a href="#"></a>perfil</li>
+    <li><a href="#"></a>Sobre</li>
+    <li><a href="#"></a>Contato</li>
+</ul>
+
+
+<hr><br><hr>
+
+<div class="container">
+    <h1>Projeto Pizzaria RadionButton e CheckBox</h1>
+    
+    <form action="" method="post">
+        
+    </form>
+</div>
+
+
+
+<?php
+
+
+?>
+</body>
+</html>
